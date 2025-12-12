f9076011bba8449421d76b4274a9c713b2c3c2e5
Andrey Romashev <andreygaranin.0@yandex.ru>
2025-12-12 14:31:45 +0300

    create swift project

---
+++ todo-swift/todo-swift/todo_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_swiftApp.swift
+//  todo-swift
+//
+//  Created by Андрей Ромашев on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct todo_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
