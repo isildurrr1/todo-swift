4ffdad462a1b14d90cc01af0f0ae7a897cc226f7
Andrey Romashev <andreygaranin.0@yandex.ru>
2025-12-28 22:01:52 +0300

    Done Checkmarks

--- todo-swift/ViewModels/ProfileViewViewModel.swift
+++ todo-swift/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Андрей Ромашев on 13.12.2025.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 import Combine
 
@@ -12,6 +14,18 @@ class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDicrionary())
     }
 }
