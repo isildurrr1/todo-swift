eff146f41aaf6f212597a8b8270ea1b55c359dcd
Andrey Romashev <andreygaranin.0@yandex.ru>
2025-12-28 18:26:32 +0300

    Saving To Do Items

--- todo-swift/ViewModels/NewItemViewViewModel.swift
+++ todo-swift/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Андрей Ромашев on 13.12.2025.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 import Combine
 
@@ -16,7 +18,29 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save () {
+        guard canSave else {
+            return
+        }
         
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDicrionary())
     }
     
     var canSave: Bool {
@@ -31,3 +55,4 @@ class NewItemViewViewModel: ObservableObject {
         return true
     }
 }
+
